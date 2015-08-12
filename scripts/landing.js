a36db0d0bb59180ded07934a67fa51d47341ee50
Dan <dvf5104@gmail.com>
2015-08-11 23:45:29 -0400

    Added addAnimate function

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,16 +1,22 @@
 var animatePoints = function () {
                 
     var points = document.getElementsByClassName('point');
-                
-    var revealPoint = function () {
-        for(i = 0; i<3; i++){
+    
+    var addAnimate = function(i){
             points[i].style.opacity = 1;
             points[i].style.transform = "scaleX(1) translateY(0)";
             points[i].style.msTransform = "scaleX(1) translateY(0)";
             points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
+    };
+    
+    
+    var revealPoint = function () {
+        for(i = 0; i < 3; i++){
+            addAnimate(i);
         }
     }; 
-    revealPoint();
+    
+    revealPoint();  
 };
 
 animatePoints();
