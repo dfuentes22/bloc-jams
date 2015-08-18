2ad05204b813e3b908938b409d22cd0847222e5d
Dan <dvf5104@gmail.com>
2015-08-17 22:26:46 -0400

    Completed Assignemnt 23, Events

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,36 +1,29 @@
 var pointsArray = document.getElementsByClassName('point');
 
-var animatePoints = function (points) {
-                
-    
-    var addAnimate = function(i){
-            points[i].style.opacity = 1;
-            points[i].style.transform = "scaleX(1) translateY(0)";
-            points[i].style.msTransform = "scaleX(1) translateY(0)";
-            points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
-    };
-    
-    
-    var revealPoint = function() {
-        for(i = 0; i < points.length; i++){
-            addAnimate(i);
-        }
-    }; 
-    
-    revealPoint();  
+
+var addAnimation = function(index){
+        pointsArray[index].style.opacity = 1;
+        pointsArray[index].style.transform = "scaleX(1) translateY(0)";
+        pointsArray[index].style.msTransform = "scaleX(1) translateY(0)";
+        pointsArray[index].style.WebkitTransform = "scaleX(1) translateY(0)";
 };
+
+
+var revealPoint = function(callback) {
+    forEach(pointsArray, callback);
+}; 
 
 
 window.onload = function() {
     //Automatically animates the points on a tall screen where scrolling isn't available
     if(window.innerHeight > 950) {
-        animatePoints(pointsArray);   
+        revealPoint(addAnimation); 
     }
   window.addEventListener('scroll', function(event) {
     //use to figure out how far the selling points are from top
     //console.log("Current offset from the top is " + pointsArray[0].getBoundingClientRect().top + " pixels");
     if(pointsArray[0].getBoundingClientRect().top <= 500) {
-        animatePoints(pointsArray);
+       revealPoint(addAnimation); 
     }
   });
 }
---
+++ scripts/utilities.js
@@ -0,0 +1,5 @@
+function forEach(array, callback){
+    for(i = 0; i < array.length; i++){
+        callback(i);
+    }
+}
