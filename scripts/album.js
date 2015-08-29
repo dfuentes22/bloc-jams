539adafa7d75d6cabdfcd6d3581652d1579023a3
Dan <dvf5104@gmail.com>
2015-08-29 17:39:26 -0400

    Completed checkpoint 26, play part 2

--- scripts/album.js
+++ scripts/album.js
@@ -85,6 +85,57 @@ var setCurrentAlbum = function(album) {
         albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].name, album.songs[i].length);   
     }
 };
+
+var findParentByClassName = function(element, targetClass) {
+    
+    var currentParent = element.parentElement;
+    
+    while (currentParent.className != targetClass) {
+        currentParent = currentParent.parentElement
+    }
+    
+    return currentParent;
+
+};
+
+var getSongItem = function(element) {
+    
+    switch (element.className) {
+        case 'album-song-button':
+        case 'ion-play':
+        case 'ion-pause':
+            return findParentByClassName(element, 'song-item-number');
+        case 'album-view-song-item':
+            return element.querySelector('.song-item-number');
+        case 'song-item-title':
+        case 'song-item-duration':
+            return findParentByClassName(element, 'album-view-song-item').querySelector('.song-item-number');
+        case 'song-item-number':
+            return element;
+        default:
+            return;
+    }
+    
+};
+
+var clickHandler = function(targetElement) {
+    var songItem = getSongItem(targetElement);
+    
+    if (currentlyPlayingSong === null) {
+        songItem.innerHTML = pauseButtonTemplate;
+        currentlyPlayingSong = songItem.getAttribute('data-song-number');
+    }
+    else if (currentlyPlayingSong === songItem.getAttribute('data-song-number')) {
+         songItem.innerHTML = playButtonTemplate;
+         currentlyPlayingSong = null;
+     }
+    else if (currentlyPlayingSong !== songItem.getAttribute('data-song-number')) {
+         var currentlyPlayingSongElement = document.querySelector('[data-song-number="' + currentlyPlayingSong + '"]');
+         currentlyPlayingSongElement.innerHTML = currentlyPlayingSongElement.getAttribute('data-song-number');
+         songItem.innerHTML = pauseButtonTemplate;
+         currentlyPlayingSong = songItem.getAttribute('data-song-number');
+     }
+ };
  
 // Elements we'll be adding listeners to
 var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
@@ -92,6 +143,10 @@ var songRows = document.getElementsByClassName('album-view-song-item');
  
  // Album button templates
 var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
+var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
+
+// Store state of playing songs
+var currentlyPlayingSong = null;
 
 window.onload = function() {
 
@@ -104,13 +159,29 @@ window.onload = function() {
         if (event.target.parentElement.className === 'album-view-song-item') {
             // Change the content from the number to the play button's HTML
             event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
+            var songItem = getSongItem(event.target);
+
+            if (songItem.getAttribute('data-song-number') !== currentlyPlayingSong) {
+                songItem.innerHTML = playButtonTemplate;
+            }
         }
     });
     for (i = 0; i < songRows.length; i++) {
-    songRows[i].addEventListener('mouseleave', function(event) {
+        songRows[i].addEventListener('mouseleave', function(event) {
         // Revert the content back to the number
-        // Selects first child element, which is the song-item-number element
-         this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
-    });
+            //#1
+             var leavingSongItem = getSongItem(event.target);
+             var leavingSongItemNumber = leavingSongItem.getAttribute('data-song-number');
+ 
+             // #2
+             if (leavingSongItemNumber !== currentlyPlayingSong) {
+                 leavingSongItem.innerHTML = leavingSongItemNumber;
+             }
+        });
+        
+        songRows[i].addEventListener('click', function(event) {
+             // Event handler call
+            clickHandler(event.target);
+        });
     }
 };
