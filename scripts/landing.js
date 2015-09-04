eeeb80e0675f7388a442aa8627dda9d990921b0b
Dan <dvf5104@gmail.com>
2015-09-03 23:36:25 -0400

    Completed checkpoint 28, refactored landing with jquery

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,29 +1,36 @@
-var pointsArray = document.getElementsByClassName('point');
+//#4
+var animatePoints = function() {
+                
+                
+    var revealPoint = function() {
+        // #6
+        $(this).css({
+            opacity: 1,
+            transform: 'scaleX(1) translateY(0)'
+        });
+    };
+    
+    // #5
+    $.each($('.point'), revealPoint);
 
-
-var addAnimation = function(index){
-        pointsArray[index].style.opacity = 1;
-        pointsArray[index].style.transform = "scaleX(1) translateY(0)";
-        pointsArray[index].style.msTransform = "scaleX(1) translateY(0)";
-        pointsArray[index].style.WebkitTransform = "scaleX(1) translateY(0)";
 };
 
-
-var revealPoint = function(callback) {
-    forEach(pointsArray, callback);
-}; 
-
-
-window.onload = function() {
-    //Automatically animates the points on a tall screen where scrolling isn't available
-    if(window.innerHeight > 950) {
-        revealPoint(addAnimation); 
+$(window).load(function() {
+    
+    // #1
+    if ($(window).height() > 950) {
+        animatePoints();
     }
-  window.addEventListener('scroll', function(event) {
-    //use to figure out how far the selling points are from top
-    //console.log("Current offset from the top is " + pointsArray[0].getBoundingClientRect().top + " pixels");
-    if(pointsArray[0].getBoundingClientRect().top <= 500) {
-       revealPoint(addAnimation); 
-    }
-  });
-}
+    
+    // #2
+    $(window).scroll(function(event) {
+        
+    // #3
+        if ($(window).scrollTop() >= 500) {
+            animatePoints();
+        
+        }
+        
+    });
+    
+});
